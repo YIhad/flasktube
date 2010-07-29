ca003ad0c9a2590f5e5405e60ed7973da7afd224
bikcmp2 <bikcmp2@gmail.com>
2010-07-29 06:15:15 +0000

    Made "/" redirect to "/upload"

--- app.py
+++ app.py
@@ -19,24 +19,10 @@ def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
 
-@app.route('/', methods=['GET', 'POST'])
-def upload_file():
-    if request.method == 'POST':
-        file = request.files['file']
-        if file and allowed_file(file.filename):
-            filename = secure_filename(file.filename)
-            file.save(os.path.join(UPLOAD_FOLDER, filename))
-            #return redirect(url_for('uploaded_file',filename=filename))
-            return "OK"
-    return '''
-    <!doctype html>
-    <title>Upload new File</title>
-    <h1>Upload new File</h1>
-    <form action="" method=post enctype=multipart/form-data>
-      <p><input type=file name=file>
-         <input type=submit value=Upload>
-    </form>
-    '''
+@app.route('/')
+def index():
+	return redirect(url_for('upload'))
+
 @app.route('/upload')
 def upload():
 	if 'username' in session:
